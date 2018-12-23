ca4d61c3f741e4741782a843c2e8dade5de253ec
jiyunn <jygr0309@gmail.com>
2018-12-23 22:19:01 +0900

    init commit

---
+++ app/src/main/java/space/jiyun/coala/MainActivity.kt
@@ -0,0 +1,19 @@
+package space.jiyun.coala
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import space.jiyun.coala.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/space/jiyun/coala/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package space.jiyun.coala.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
