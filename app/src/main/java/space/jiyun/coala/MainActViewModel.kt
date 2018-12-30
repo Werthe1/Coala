96d0ad47e7aa8ed14eba346146a07afd580fc1b4
Jiyunn <jygr0309@gmail.com>
2018-12-30 23:55:30 +0900

    #9-Add BindingAdapter to set selectedItem.

--- app/src/main/java/space/jiyun/coala/MainActViewModel.kt
+++ app/src/main/java/space/jiyun/coala/MainActViewModel.kt
@@ -1,11 +1,23 @@
 package space.jiyun.coala
 
 import android.view.MenuItem
+import androidx.annotation.IdRes
 import androidx.databinding.BindingAdapter
 import androidx.databinding.BindingMethod
 import androidx.databinding.BindingMethods
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import space.jiyun.coala.databinding.AnswerFragmentBinding
+import space.jiyun.coala.ui.answer.AnswerFragment
+import space.jiyun.coala.ui.home.HomeFragment
+import space.jiyun.coala.ui.question.QuestionFragment
+
+@BindingAdapter("selectedItemPosition")
+fun setSelectedItemPosition(view : BottomNavigationView, @IdRes itemId : Int) {
+    view.selectedItemId = itemId
+}
 
 @BindingMethods(
         BindingMethod(
