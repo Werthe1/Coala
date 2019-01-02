cb91bbce9a1c077dd4325cedf256ad72495da2f9
jiyunn <jygr0309@gmail.com>
2019-01-02 14:00:28 +0900

    #22-Add openImgPicker method.

--- app/src/main/java/space/jiyun/coala/ui/writequestion/WriteQuestionNavigator.kt
+++ app/src/main/java/space/jiyun/coala/ui/writequestion/WriteQuestionNavigator.kt
@@ -1,7 +1,12 @@
 package space.jiyun.coala.ui.writequestion
 
+import android.view.View
+import android.widget.ImageView
+import androidx.fragment.app.FragmentManager
+
 interface WriteQuestionNavigator {
 
     fun addTag()
 
+    fun openImagePicker(position : Int)
 }
