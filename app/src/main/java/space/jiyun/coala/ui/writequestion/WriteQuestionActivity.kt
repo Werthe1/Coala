4e8b18b6e447912634eeb31127856fdb57fbd44d
jiyunn <jygr0309@gmail.com>
2019-01-02 14:00:56 +0900

    #22-Add photo registration.

--- app/src/main/java/space/jiyun/coala/ui/writequestion/WriteQuestionActivity.kt
+++ app/src/main/java/space/jiyun/coala/ui/writequestion/WriteQuestionActivity.kt
@@ -1,15 +1,22 @@
 package space.jiyun.coala.ui.writequestion
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
-import co.lujun.androidtagview.TagView
+import com.bumptech.glide.request.RequestOptions
+import gun0912.tedbottompicker.TedBottomPicker
 import space.jiyun.coala.R
 import space.jiyun.coala.databinding.WriteQuestionActivityBinding
+import space.jiyun.coala.util.GlideApp
 import space.jiyun.coala.util.setupActionBar
 
 class WriteQuestionActivity : AppCompatActivity(), WriteQuestionNavigator {
@@ -17,6 +24,7 @@ class WriteQuestionActivity : AppCompatActivity(), WriteQuestionNavigator {
     private lateinit var binding: WriteQuestionActivityBinding
     private lateinit var viewModel: WriteQuestionViewModel
 
+    private val storagePermissionCode = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +54,43 @@ class WriteQuestionActivity : AppCompatActivity(), WriteQuestionNavigator {
         }
     }
 
+
+    override fun openImagePicker(position: Int) {
+        checkStorageAccessPermission()
+
+        TedBottomPicker.Builder(this)
+                .setOnImageSelectedListener {
+                    viewModel.setImgUri(position, it.toString())
+                }
+                .setImageProvider { imageView, imageUri ->
+                    GlideApp
+                            .with(this)
+                            .load(imageUri)
+                            .thumbnail(0.1f)
+                            .apply(RequestOptions().centerCrop())
+                            .into(imageView)
+                }
+                .setPreviewMaxCount(7)
+                .create()
+                .show(supportFragmentManager)
+    }
+
+
+    private fun checkStorageAccessPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  // >= 23
+            val requestPermission = Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+            ContextCompat.checkSelfPermission(this, requestPermission).run {
+
+                if (this != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this@WriteQuestionActivity,
+                            arrayOf(requestPermission), storagePermissionCode)
+                }
+            }
+        }
+    }
+
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu_write, menu)
         return true
@@ -60,5 +105,14 @@ class WriteQuestionActivity : AppCompatActivity(), WriteQuestionNavigator {
                 }
                 else -> super.onOptionsItemSelected(item)
             }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, results: IntArray) {
+        if (results.isNotEmpty() && results[0] == PackageManager.PERMISSION_GRANTED) {
+            when (requestCode) {
+                storagePermissionCode -> return
+                else -> super.onRequestPermissionsResult(requestCode, permissions, results)
+            }
+        }
+    }
 
 }
--- app/src/main/java/space/jiyun/coala/ui/writequestion/WriteQuestionViewModel.kt
+++ app/src/main/java/space/jiyun/coala/ui/writequestion/WriteQuestionViewModel.kt
@@ -1,24 +1,43 @@
 package space.jiyun.coala.ui.writequestion
 
 import android.app.Application
-import android.util.Log
-import android.view.View
-import androidx.databinding.BindingMethod
-import androidx.databinding.BindingMethods
 import androidx.databinding.ObservableArrayList
 import androidx.lifecycle.AndroidViewModel
-import co.lujun.androidtagview.TagContainerLayout
-import co.lujun.androidtagview.TagView
+import androidx.databinding.ObservableField
 
 
-class WriteQuestionViewModel(val context : Application) : AndroidViewModel(context) {
+class WriteQuestionViewModel(val context: Application) : AndroidViewModel(context) {
 
-    var navigator : WriteQuestionNavigator?=null
+    var navigator: WriteQuestionNavigator? = null
     val tags = ObservableArrayList<String>()
+
+    val imgUri1 = ObservableField<String>()
+    val imgUri2 = ObservableField<String>()
+    val imgUri3 = ObservableField<String>()
 
 
     fun onAddTagClick() {
         navigator?.addTag()
+    }
+
+    fun onDeleteImgClick(position: Int) {
+        when (position) {
+            1 -> imgUri1.set(null)
+            2 -> imgUri2.set(null)
+            3 -> imgUri3.set(null)
+        }
+    }
+
+    fun onSelectImgClick(position: Int) {
+        navigator?.openImagePicker(position)
+    }
+
+    fun setImgUri(position: Int, uri: String) {
+        when (position) {
+            1 -> imgUri1.set(uri)
+            2 -> imgUri2.set(uri)
+            3 -> imgUri3.set(uri)
+        }
     }
 
 
