f2b536b1a2b634bf131818bf8312a65d2d0a328f
jiyunn <jygr0309@gmail.com>
2019-01-06 21:03:28 +0900

    #37-Initalize realm in Application class.

--- app/src/main/java/space/jiyun/coala/CoalaApplication.kt
+++ app/src/main/java/space/jiyun/coala/CoalaApplication.kt
@@ -2,6 +2,9 @@ package space.jiyun.coala
 
 import android.app.Application
 import com.bumptech.glide.Glide
+import com.facebook.stetho.Stetho
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider
+import io.realm.Realm
 
 class CoalaApplication : Application() {
 
@@ -9,6 +12,14 @@ class CoalaApplication : Application() {
         super.onCreate()
 
         Glide.with(this)
+        Realm.init(this)
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(
+                                RealmInspectorModulesProvider.builder(this).build())
+                        .build())
     }
 
 }
